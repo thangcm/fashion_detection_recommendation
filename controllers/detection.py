6a5e9633a9505ff4660ef0fc7cb2cac872079fea
thangcm <chu.minh.thang@vsi-international.com>
2020-09-18 15:51:03 +0700

    First commit

---
+++ controllers/detection.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import os
+import time
+
+def detect_image(img_name, img_path, weight_path, config_path):
+    net = cv2.dnn.readNetFromDarknet(config_path, weight_path)
+    img = cv2.imread(img_path+img_name)
+
+    (H, W) = img.shape[:2]
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > 0.5:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+            
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
+    
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+
+    output_name = img_name.replace('.', '_detected.')
+    output_path = img_path + output_name
+    cv2.imwrite(output_path, img)
+    return output_path
---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for, request
+from controllers import detection
+import os
+app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = "static/"
+
+@app.route("/", methods=["GET", "POST"])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url + "detection?filename=" + filename)
+
+            filename = image.filename
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+            print("Image saved")
+            return redirect(request.url + "detection?filename=" + filename)
+
+    return render_template("upload_image.html")
+
+
+@app.route("/detection", methods=["GET"])
+def detect_image():
+    filename = request.args.get("filename")
+    weights_path = os.path.join(app.config["IMAGE_UPLOADS"], 'models/yolov3-df2_15000.weights')
+    config_path = os.path.join(app.config["IMAGE_UPLOADS"], 'models/yolov3-df2.cfg')
+    detected_image_path = detection.detect_image(filename, app.config["IMAGE_UPLOADS"], weights_path, config_path)
+    return render_template("show_detection.html", user_image = detected_image_path)
+
+
+if __name__ == '__main__':
+   app.run()
